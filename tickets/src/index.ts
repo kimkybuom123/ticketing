f481e419a11955189f59db3bddcff31e902c71ac
kimkybuom123 <64270942+kimkybuom123@users.noreply.github.com>
2021-03-22 14:34:25 +0700

    Update index.ts

--- tickets/src/index.ts
+++ tickets/src/index.ts
@@ -4,7 +4,7 @@ import { app } from './app';
 
 const start = async () => {
   if (!process.env.JWT_KEY) {
-    throw new Error('JWT_KEY must be defined');
+    throw new Error('JWT_KEY must be defined ok o k');
   }
   if (!process.env.MONGO_URI) {
     throw new Error('MONGO_URI must defined okok');
