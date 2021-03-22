38e0fd12d018bb78b1a3ab01785ffd582fa83613
kimkybuom123 <64270942+kimkybuom123@users.noreply.github.com>
2021-03-23 02:18:36 +0700

    Update index.ts

--- tickets/src/index.ts
+++ tickets/src/index.ts
@@ -16,7 +16,7 @@ const start = async () => {
       useUnifiedTopology: true,
       useCreateIndex: true,
     });
-    console.log('Connected   mongoDb');
+    console.log('Connected to  mongoDb');
   } catch (err) {
     console.error(err);
   }
